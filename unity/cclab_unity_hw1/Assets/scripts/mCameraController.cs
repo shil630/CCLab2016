77f24f49c1545f604b1e391b17057972f1723c15
shil630 <shiliangbj12@gmail.com>
2016-11-15 00:54:47 -0500

    add unity hw1

---
+++ unity/cclab_unity_hw1/Assets/scripts/mCameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mCameraController : MonoBehaviour {
+	
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	
+	}
+
+	// folloe Cameras, Procedural Animations, etc.
+	// LateUpdate, runs after all items have been processed
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ unity/cclab_unity_hw1/Assets/scripts/playerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float speed;
+	public Text scoreText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		score = 0;
+		setScoreText ();
+		winText.text = "";
+	
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive (false);	
+			score += 10;
+			setScoreText ();
+		}
+	}
+
+	void setScoreText () {
+		scoreText.text = "score:" + score.ToString ();
+
+		if(score >= 200) {
+			winText.text = "You Win!";
+		}
+
+	}
+}
